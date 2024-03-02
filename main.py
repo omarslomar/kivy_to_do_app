a98844445291c7d355e6e40667406b49ecd2214e
Omar <porven.omar@gmail.com>
2024-03-02 17:45:18 -0500

    new button

--- main.py
+++ main.py
@@ -4,18 +4,33 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 
 import sys
 
-def clicked():
-    print("Who told you to click me?")
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super(MyWindow, self).__init__()
+        self.setGeometry(1300, 500, 500, 500)
+        self.setWindowTitle("To Do List")
+        self.initUI()
+    
+    def initUI(self):
+        self.label = QtWidgets.QLabel(self)
+        self.label.setText("Balls")
+        self.label.move(100, 100)
+
+        self.b1 = QtWidgets.QPushButton(self)
+        self.b1.setText("Click Me!")
+        self.b1.clicked.connect(self.clicked)
+
+    def clicked(self):
+        self.label.setText("You have pressed the button, bitch")
+        self.update()
+
+    def update(self):
+        self.label.adjustSize()
+
 
 def window():
     app = QApplication(sys.argv)
-    win = QMainWindow()
-    win.setGeometry(1300, 500, 500, 500)
-    win.setWindowTitle("To Do List")
-
-    label = QtWidgets.QLabel(win)
-    label.setText("Balls")
-    label.move(100, 100)
+    win = MyWindow()
 
     win.show()
     sys.exit(app.exec_())
