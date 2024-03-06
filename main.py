4d9040afd46637659cd0a806bf47c9b973bcda09
Omar <porven.omar@gmail.com>
2024-03-06 07:23:23 -0500

    restart yet again

--- main.py
+++ main.py
@@ -1,12 +1,41 @@
-import kivy
-from kivy.app import App
-from kivy.uix.label import Label
+import PyQt5
+from PyQt5 import QtWidgets
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
-class MyApp(App):
-    def build(self):
-        return Label(text="New App")
-    
+import sys
+
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super(MyWindow, self).__init__()
+        self.setGeometry(1300, 500, 500, 500)
+        self.setWindowTitle("To Do List")
+        self.initUI()
+
+    def initUI(self):
+        self.label = QtWidgets.QLabel(self)
+        self.label.setText("Balls")
+        self.label.move(100, 100)
+
+        self.b1 = QtWidgets.QPushButton(self)
+        self.b1.setText("Click Me!")
+        self.b1.clicked.connect(self.clicked)
+
+    def clicked(self):
+        self.label.setText("You have pressed the button, bitch")
+        self.update()
+
+    def update(self):
+        self.label.adjustSize()
+
+
+def window():
+    app = QApplication(sys.argv)
+    win = MyWindow()
+
+    win.show()
+    sys.exit(app.exec_())
 
 
 if __name__ == "__main__":
-    MyApp().run()
+    window()
