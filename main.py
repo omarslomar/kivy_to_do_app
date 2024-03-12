c930fdd8d333412f2f5035817af83b6c9604e66a
Omar <porven.omar@gmail.com>
2024-03-12 13:31:26 -0400

    YESSSSS

--- main.py
+++ main.py
@@ -6,52 +6,81 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 
 
+# Main class. Actually implements everything
 class MyGrid(GridLayout):
     def __init__(self, **kwargs):
         super(MyGrid, self).__init__(**kwargs)
 
+        """States that the whole thing is one columns (task managers and big submit are on 2 separate grids)"""
         self.cols = 1
 
-        self.inside = GridLayout()
-        self.inside.cols = 2
+        """Initializes the first 'to do' task for MAC"""
+        self.MAC = GridLayout()
+        self.MAC.cols = 3
 
-        self.inside.add_widget(Label(text="Frist Name:"))
-        self.first_name = TextInput(multiline=False)
-        self.inside.add_widget(self.first_name)
+        self.MAC.add_widget(Label(text="For MAC1147, PRECAL ALG TRIG", font_size=20))
+        self.to_do_MAC = TextInput(multiline=False)
+        self.finish_MAC = Button(text="Complete Task", font_size=30)
 
-        self.inside.add_widget(Label(text="Last Name:"))
-        self.last_name = TextInput(multiline=False)
-        self.inside.add_widget(self.last_name)
+        """Finish button for this task. binds it to the functions in the botton"""
+        self.finish_MAC.bind(on_press=self.MAC_small_button)
+        self.MAC.add_widget(self.to_do_MAC)
+        self.MAC.add_widget(self.finish_MAC)
 
-        self.inside.add_widget(Label(text="Age:"))
-        self.age = TextInput(multiline=False)
-        self.inside.add_widget(self.age)
+       
+        """Initializes the first 'to do' task for POS"""
+        self.POS = GridLayout()
+        self.POS.cols = 3
 
-        self.add_widget(self.inside)
+        self.POS.add_widget(Label(text="For POS2041, AMER NT GOVERN", font_size=20))
+        self.to_do_POS = TextInput(multiline=False)
+        self.finish_POS = Button(text="Complete Task", font_size=30)
+        self.finish_POS.bind(on_press=self.POS_small_button)
+        self.POS.add_widget(self.to_do_POS)
+        self.POS.add_widget(self.finish_POS)
 
-        self.submit = Button(text="Submit", font_size=40)
-        self.submit.bind(on_press=self.pressed)
-        self.add_widget(self.submit)
+        """Initializes the first 'to do' task for EVR"""
+        self.EVR = GridLayout()
+        self.EVR.cols = 3
+
+        self.EVR.add_widget(Label(text="For EVR21001, ENVIO SCI", font_size=20))
+        self.to_do_EVR = TextInput(multiline=False)
+        self.finish_EVR = Button(text="Complete Task", font_size=30)
+        self.finish_EVR.bind(on_press=self.EVR_small_button)
+        self.EVR.add_widget(self.to_do_EVR)
+        self.EVR.add_widget(self.finish_EVR)
+
+        """Actially adds all to the actual main GridLayout() object"""
+        self.add_widget(self.MAC)
+        self.add_widget(self.POS)
+        self.add_widget(self.EVR)
+
+        """Creates and adds submit button to main GridLayout()"""
+        self.finish_all = Button(text="Finish All", font_size=40)
+        self.finish_all.bind(on_press=self.all_done_button)
+        self.add_widget(self.finish_all)
+
+
+    """All button press functions. for minor presses and the one big one"""
+    def MAC_small_button(self, instance):
+        print(f"Done! Completed task: {self.to_do_MAC.text}")
+
+    def POS_small_button(self, instance):
+        print(f"Done! Completed task: {self.to_do_POS.text}")
     
-    def pressed(self, instance):
-        first = self.first_name.text
-        last = self.last_name.text
-        name = f"{first} {last}"
-        age = self.age.text
+    def EVR_small_button(self, instance):
+        print(f"Done! Completed task: {self.to_do_EVR.text}")
 
-        self.first_name.text = ""
-        self.last_name.text = ""
-        self.age.text = ""
+    def all_done_button(self, instance):
+        message = ("Congratulations! You have completed all your tasks!")
+        print(message)
 
-        print(f"Name: {name.title()}")
-        print(f"Age: {age}")
-
-
+# I think this just displays the display on my screen
 class MyApp(App):
     def build(self):
         return MyGrid()
     
 
-
+# runs the whole ting
 if __name__ == "__main__":
     MyApp().run()
--- readme.md
+++ readme.md
@@ -3,3 +3,6 @@ Use this branch to actually inplement what you want. The other is for following 
 
 
 bro actually fucking document this time
+
+I think i'm done for now. I just need to implement all the cool little features I wnt. 
+I have to folloow the tutorial for that
