aff1b9d1a87b550e5e87bd60b7adab00443f1eaf
Omar <porven.omar@gmail.com>
2024-03-13 09:52:09 -0400

    new instances

--- main.py
+++ main.py
@@ -50,10 +50,34 @@ class MyGrid(GridLayout):
         self.EVR.add_widget(self.to_do_EVR)
         self.EVR.add_widget(self.finish_EVR)
 
+        """Initializes the first 'to do' task for coding"""
+        self.CODING = GridLayout()
+        self.CODING.cols = 3
+
+        self.CODING.add_widget(Label(text="Coding", font_size=20))
+        self.to_do_CODING = TextInput(multiline=False)
+        self.finish_CODING = Button(text="Complete Task", font_size=30)
+        self.finish_CODING.bind(on_press=self.CODING_small_button)
+        self.CODING.add_widget(self.to_do_CODING)
+        self.CODING.add_widget(self.finish_CODING)
+
+        """Initializes the first 'to do' task for anything else"""
+        self.EXTRA = GridLayout()
+        self.EXTRA.cols = 3
+
+        self.EXTRA.add_widget(Label(text="Anything Else", font_size=20))
+        self.to_do_EXTRA = TextInput(multiline=False)
+        self.finish_EXTRA = Button(text="Complete Task", font_size=30)
+        self.finish_EXTRA.bind(on_press=self.EXTRA_small_button)
+        self.EXTRA.add_widget(self.to_do_EXTRA)
+        self.EXTRA.add_widget(self.finish_EXTRA)
+
         """Actially adds all to the actual main GridLayout() object"""
         self.add_widget(self.MAC)
         self.add_widget(self.POS)
         self.add_widget(self.EVR)
+        self.add_widget(self.CODING)
+        self.add_widget(self.EXTRA)
 
         """Creates and adds submit button to main GridLayout()"""
         self.finish_all = Button(text="Finish All", font_size=40)
@@ -70,6 +94,12 @@ class MyGrid(GridLayout):
     
     def EVR_small_button(self, instance):
         print(f"Done! Completed task: {self.to_do_EVR.text}")
+
+    def CODING_small_button(self, instance):
+        print(f"Done! Completed task: {self.to_do_CODING.text}")
+
+    def EXTRA_small_button(self, instance):
+        print(f"Done! Completed task: {self.to_do_EXTRA.text}")
 
     def all_done_button(self, instance):
         message = ("Congratulations! You have completed all your tasks!")
